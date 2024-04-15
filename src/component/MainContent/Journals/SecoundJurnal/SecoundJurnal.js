cfa38b8f63c0240966d2387286b0403a39cf89b6
maede motlagh <maede.mo15@gmail.com>
2024-04-15 16:54:17 +0330

    done animation firstLoading

--- src/component/MainContent/Journals/SecoundJurnal/SecoundJurnal.js
+++ src/component/MainContent/Journals/SecoundJurnal/SecoundJurnal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './style.scss';
 
 
@@ -8,18 +8,67 @@ const SecoundJurnal = () => {
     const desc = "A complete line of shampoo and mask for cleansing, nourishing and restructuring the hair fiber without changing its natural balance."
     const btnText = "Discover me"
 
+    const [animate , setAnimate] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const componentImg = document.getElementById('imgJurnalTwo');
+          if (componentImg) {
+            const rect = componentImg.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight;
+            if(isVisible && !animate){
+                setAnimate(isVisible);
+            }
+          }
+
+          const componentTitleTwo = document.getElementById('titleJurnalTwo');
+          if (componentTitleTwo) {
+            const rect = componentTitleTwo.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight;
+            if(isVisible && !animate){
+                setAnimate(isVisible);
+            }
+          }
+
+          const componentDescTwo = document.getElementById('descJurnalTwo');
+          if (componentDescTwo) {
+            const rect = componentDescTwo.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight;
+            if(isVisible && !animate){
+                setAnimate(isVisible);
+            }
+          }
+
+          const componentBtnTwo = document.getElementById('btnJurnalTwo');
+          if (componentBtnTwo) {
+            const rect = componentBtnTwo.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight;
+            if(isVisible && !animate){
+                setAnimate(isVisible);
+            }
+          }
+
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+
 
     return(
         <div className="secondContainer" >
             <div className="secondContainer__left" >
                 <div style={{display : 'flex' , flexDirection : 'column'}} >
-                    <span>{title}</span>
-                    <p>{desc}</p>
-                    <button>{btnText}</button>
+                    <span id="titleJurnalTwo" className={animate && 'animateTitleTwo'} >{title}</span>
+                    <p id="descJurnalTwo" className={animate && 'animateDescTwo'} >{desc}</p>
+                    <button id="btnJurnalTwo" className={animate && 'animateBtnTwo'}>{btnText}</button>
                 </div>
                 
             </div>
-            <div className="secondContainer__right" >
+            <div id="imgJurnalTwo" className={`secondContainer__right ${animate && 'animateImgTwo'}`} >
                 <img src="img/bluegirl.jpg" />
             </div>
         </div>
--- src/component/MainContent/Journals/ThirdJurnal/ThirdJurnal.js
+++ src/component/MainContent/Journals/ThirdJurnal/ThirdJurnal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './style.scss';
 
 
@@ -8,18 +8,65 @@ const ThirdJurnal = () => {
     const desc = "Whether you want to create a new style in the Salon or directly at Home, here are the styling products to create the look you want."
     const btnText = "Discover me"
 
+    const [animate , setAnimate] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const componentImgThree = document.getElementById('imgAnimateThree');
+          if (componentImgThree) {
+            const rect = componentImgThree.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight;
+            if(isVisible && !animate){
+                setAnimate(isVisible);
+            }
+          }
+
+          const componentTitleThree = document.getElementById('animateTitleThree');
+          if (componentTitleThree) {
+            const rect = componentTitleThree.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight;
+            if(isVisible && !animate){
+                setAnimate(isVisible);
+            }
+          }
+
+          const componentDescThree = document.getElementById('animateDescThree');
+          if (componentDescThree) {
+            const rect = componentDescThree.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight;
+            if(isVisible && !animate){
+                setAnimate(isVisible);
+            }
+          }
+
+          const componentBtnThree = document.getElementById('animateBtnThree');
+          if (componentBtnThree) {
+            const rect = componentBtnThree.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight;
+            if(isVisible && !animate){
+                setAnimate(isVisible);
+            }
+          }
+
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
 
     return(
         <div className="thirdContainer" >
             <div className="thirdContainer__left" >
                 <div style={{display : 'flex' , flexDirection : 'column' , direction: 'ltr'}} >
-                    <span>{title}</span>
-                    <p>{desc}</p>
-                    <button>{btnText}</button>
+                    <span id="animateTitleThree" className={animate && "activeTitleThree"} >{title}</span>
+                    <p id="animateDescThree"  className={animate && "activeDescThree"}>{desc}</p>
+                    <button id="animateBtnThree"  className={animate && "activeBtnThree"}>{btnText}</button>
                 </div>
                 
             </div>
-            <div className="thirdContainer__right" >
+            <div id="imgAnimateThree" className={`thirdContainer__right ${animate && "animateImgThree"}`} >
                 <img src="img/pinkgirl.jpg" />
             </div>
         </div>
