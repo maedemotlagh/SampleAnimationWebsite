0aeab33ea645966967fbbd049e57d2a94de5c864
maede motlagh <maede.mo15@gmail.com>
2024-05-01 18:01:57 +0330

    responsive

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -95,9 +95,19 @@ const Navbar = () => {
             <a className={`${activeChange && 'activeChange'} navbar__btn`} >
                 {ContactUsText}
             </a>
+
+            <a className={`${activeChange && 'activeChange'} navbar__mobileMenu`} >
+                {/* {ContactUsText} */}
+                <MenuIcon/>
+            </a>
             
         </nav>
     )
 }
+
+
+
+const MenuIcon = () => <svg version="1.1" id="Layer_1"  x="0px" y="0px"viewBox="0 0 50 50" enable-background="new 0 0 50 50" ><path fill="#231F20" d="M8.667,15h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,15,8.667,15z"/><path fill="#231F20" d="M8.667,37h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,37,8.667,37z"/><path fill="#231F20" d="M8.667,26h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,26,8.667,26z"/></svg>
+
 
 export default Navbar
